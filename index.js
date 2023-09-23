bc973ba624b32621d07a1be3dd0388049c3057de
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2023-09-23 20:58:03 +1000

    Vega lite shows bar graph now

---
+++ index.js
@@ -0,0 +1,4 @@
+var map = "mapdata.vg.json";
+vegaEmbed('#map', map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

b6f272c107c08e0205e36761da7b80c2f12ce6db
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2023-09-28 17:28:56 +1000

    Added manufacturing location map

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var map = "mapdata.vg.json";
+var map = "salesMapData.vg.json";
+var factoryMap = "factoryMapData.vg.json";
 vegaEmbed('#map', map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed('#factory-location-map', factoryMap).then(function(result) {
+    // Access vega view
 }).catch(console.error);

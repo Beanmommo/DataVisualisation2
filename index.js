4e20af3f45831df653eb7d995bdb641a2136bb74
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2023-10-03 23:51:46 +1100

    Added pictures to be used in top10cars

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var map = "salesMapData.vg.json";
 var factoryMap = "factoryMapData.vg.json";
 var salesProdBarGraph = "salesProductionGraphs.vg.json"
+var top10car = "top10CarGraph.vg.json"
 vegaEmbed('#factory-location-map', factoryMap).then(function(result) {
     // Access vega view
 }).catch(console.error);
 vegaEmbed('#prod-sales-graph', salesProdBarGraph).then(function(result) {
+
+}).catch(console.error);
+vegaEmbed('#top10car', top10car).then(function(result) {
 
 }).catch(console.error);

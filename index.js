61798ec8e7e5fb47dce464c63a9163988786990b
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2023-10-03 17:34:05 +1100

    Commit a lot of stuff

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var map = "salesMapData.vg.json";
 var factoryMap = "factoryMapData.vg.json";
-vegaEmbed('#map', map).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var salesProdBarGraph = "salesProductionGraphs.vg.json"
 vegaEmbed('#factory-location-map', factoryMap).then(function(result) {
     // Access vega view
+}).catch(console.error);
+vegaEmbed('#prod-sales-graph', salesProdBarGraph).then(function(result) {
+
 }).catch(console.error);
---
+++ production_sales_data_emerging.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+production = pd.read_csv('toyota-country-production.csv')
+sales = pd.read_csv('toyota-country-sakes.csv')
+

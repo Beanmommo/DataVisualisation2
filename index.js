2bb9f1d7c024642c52fdf0f37d318c04078e6063
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2023-10-05 00:56:34 +1100

    Added params, added salesProd map, todo: Create another layer for "Others" in sales as the region

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var salesProdBarGraph = "salesProductionGraphs.vg.json"
 var top10car = "top10CarGraph.vg.json"
 var marketCapLineGraph = "marketCapLineGraph.vg.json"
 var marketCap2023BarGraph = "marketCap2023Graph.vg.json"
+var salesProdMap = "salesProductionMapData.vg.json"
+console.log("Give Leonardo Prasetyo 30369908 High HD Please :)")
 vegaEmbed('#factory-location-map', factoryMap).then(function(result) {
     // Access vega view
 }).catch(console.error);
@@ -17,5 +19,8 @@ vegaEmbed('#top10market', marketCapLineGraph).then(function(result) {
 
 }).catch(console.error);
 vegaEmbed('#top10autocompany', marketCap2023BarGraph).then(function(result) {
+
+}).catch(console.error);
+vegaEmbed('#production-sales-country-map', salesProdMap).then(function(result) {
 
 }).catch(console.error);

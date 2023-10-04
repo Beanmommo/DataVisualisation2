3f1bd567b4e7434bdb8f82cb13e663bd7c1fcbe0
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2023-10-04 20:00:45 +1100

    Moved allgraph into layers for text annotation except productionsales graph

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var factoryMap = "factoryMapData.vg.json";
 var salesProdBarGraph = "salesProductionGraphs.vg.json"
 var top10car = "top10CarGraph.vg.json"
 var marketCapLineGraph = "marketCapLineGraph.vg.json"
+var marketCap2023BarGraph = "marketCap2023Graph.vg.json"
 vegaEmbed('#factory-location-map', factoryMap).then(function(result) {
     // Access vega view
 }).catch(console.error);
@@ -13,5 +14,8 @@ vegaEmbed('#top10car', top10car).then(function(result) {
 
 }).catch(console.error);
 vegaEmbed('#top10market', marketCapLineGraph).then(function(result) {
+
+}).catch(console.error);
+vegaEmbed('#top10autocompany', marketCap2023BarGraph).then(function(result) {
 
 }).catch(console.error);

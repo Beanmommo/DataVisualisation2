72cf87a43a659e206f35ab1f6eccab472e873963
Leonardo Prasetyo <leonardo.prasetyo5@gmail.com>
2023-10-10 11:37:22 +1100

    Comited studd

--- Algorithms/populate-country.py
+++ Algorithms/populate-country.py
@@ -1,6 +1,15 @@
 import csv
 
 resultFileName = 'result.csv'
+
+countryInContinent = {
+    "Africa": 51,
+    "Asia": 47,
+    "Europe": 39,
+    "South America": 13,
+    "North America": 18,
+    "Oceania": 7    
+}
 #Open csv file
 with open('other_continent_country.csv', mode='r') as file:
     csvreader = csv.reader(file)
@@ -15,6 +24,7 @@ with open('other_continent_country.csv', mode='r') as file:
             year = row[3]
             value = row[4]
             with open(continent+'.txt') as continentFile:
+                value = round(int(float(value)) / countryInContinent[continent], 1)
                 for country in continentFile:
                     country = country.replace('\n', '')
                     csvline = []
